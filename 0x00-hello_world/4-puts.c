8afb733cf9eb6e19f1a5d168d66fba90590c5889
Luvo-Codes <lngodwane@gmail.com>
2023-06-19 21:37:43 +0200

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: Prints statements using puts method
+ *
+ * Return: always 0 (success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
