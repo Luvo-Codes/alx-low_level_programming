f7f7387b8c27c623561446a1f9656682c10ad70d
Luvo-Codes <lngodwane@gmail.com>
2023-06-19 23:35:37 +0200

    101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: Should print string to stderr
+ *
+ * Return: always 1
+ */
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - \
+			 Dora Korpar, 2015-10-19\n";
+
+	write(2, message, 59);
+	return (1);
+}
