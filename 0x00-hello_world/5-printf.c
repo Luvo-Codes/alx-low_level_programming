37a531a4df879f49435bdf65ede46e8ccab25be4
Luvo-Codes <lngodwane@gmail.com>
2023-06-19 22:25:34 +0200

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints a statement using printf
+ *
+ * Return: always returns 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
