2e90185afb7cbed3f7a755cdc9472f404f3d2c01
Luvo-Codes <lngodwane@gmail.com>
2023-06-21 02:58:21 +0200

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints base10 single digits
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
