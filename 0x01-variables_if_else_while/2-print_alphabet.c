9dc4f2f2be87dc6579172440c1036e8d430874ce
Luvo-Codes <lngodwane@gmail.com>
2023-06-21 02:16:02 +0200

    Allphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet followed by a new line
+ *
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a', letter <= z; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
