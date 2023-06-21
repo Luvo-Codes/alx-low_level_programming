8caf7cc2b0b2f6d063b1d82cb3649340e512de58
Luvo-Codes <lngodwane@gmail.com>
2023-06-21 03:47:40 +0200

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints combinations from 1-9
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar('0' + digit);
+
+		if (digit != 9)
+			putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
