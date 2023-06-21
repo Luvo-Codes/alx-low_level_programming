0d29569bcc9ced2e3ff124fa3cafbd7cf1119aba
Luvo-Codes <lngodwane@gmail.com>
2023-06-21 02:43:35 +0200

    Allphabts

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints base10 single digits
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		printf(i);
+	return (0);
+}
