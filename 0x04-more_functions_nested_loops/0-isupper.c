e3bba9e66d7cc93352c39ebc21f0f70c5c151f81
Luvo-Codes <lngodwane@gmail.com>
2023-06-23 00:19:37 +0200

    _isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,29 +1,4 @@
 #include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Description: prints 1 if character is uppercase. Prints 2 otherwise
- *
- * @c: the character to be checked
- *
- * Return: always 0.
- */
-int _isupper(int c);
-
-int main(void)
-{
-	char c;
-
-	scnaf("%C", &c);
-	if (_isupper(c))
-	{
-		printf("%c: %d\n", c, _isupper(c));
-		return (1);
-	}
-	return (0);
-}
-
 /**
  * _isupper - checks if a character is upper or lowercase
  *
